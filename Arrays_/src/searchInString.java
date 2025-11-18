f841a9de278b635d07dd3153db56ce3a33b9201d
Namir <namir.codes@gmail.com>
2025-11-18 14:11:04 +0530

    Searching in String

---
+++ Arrays_/src/searchInString.java
@@ -0,0 +1,11 @@
+public class searchInString {
+
+    public static void main(String[] args) {
+
+        String name = "Namir";
+        System.out.println(search(name, 'a'));
+    }
+
+    static boolean search(String str, char target) {
+    }
+}
